3b0e52b170695eb619ca2fdf5cf7556ceacd54ca
Darlington Olelewe <darlington.olelewe@decagon.dev>
2022-05-20 02:40:41 +0100

    my first commit

---
+++ src/main/java/com/examine/crosschecked/CrossCheckedApplication.java
@@ -0,0 +1,13 @@
+package com.examine.crosschecked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossCheckedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossCheckedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/examine/crosschecked/CrossCheckedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examine.crosschecked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossCheckedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
